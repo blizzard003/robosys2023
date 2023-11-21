c61695137a0966330e7602c808681c1a696c014e
Yuki Kasama <blizzard03@kasama>
2023-11-21 12:22:39 +0900

    Add a test script

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash -xv
+#!/bin/bash 
 #SPDX-FileCopyrightTest: 2023 Yuki Kasama
 #SPDX-Licence-Identifier: BSD-3-Clause
 
@@ -15,11 +15,11 @@ out=$(seq 5 | ./plus)
 
 ### STRANGE INPUT
 out=$(echo あ | ./plus)
-[ "$?" = 1 ]      :|| ng ${LINENO}
+[ "$?" = 1 ] || ng ${LINENO}
 [ "${out}" = "" ] || ng ${LINENO}
 
 out=$(echo  | ./plus)
-[ "$?" = 1 ]      || ng ${LINENO}
+[ "$?" = 1 ] || ng ${LINENO}
 [ "${out}" = "" ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
