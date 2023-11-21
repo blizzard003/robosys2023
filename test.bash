67fdd438311dbfaf818b078619fff096ac87fe02
Yuki Kasama <blizzard03@kasama>
2023-11-21 11:49:07 +0900

    Add a test script

--- test.bash
+++ test.bash
@@ -15,11 +15,11 @@ out=$(seq 5 | ./plus)
 
 ### STRANGE INPUT
 out=$(echo あ | ./plus)
-[ "$?" = 1 ] || ng ${LINENO}
+[ "$?" = 1 ]      :|| ng ${LINENO}
 [ "${out}" = "" ] || ng ${LINENO}
 
 out=$(echo  | ./plus)
-[ "$?" = 1 ] || ng ${LINENO}
+[ "$?" = 1 ]      || ng ${LINENO}
 [ "${out}" = "" ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
