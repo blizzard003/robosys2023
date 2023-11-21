76df1b6cd73e7a9b68a6d5c043357776466f66c1
Yuki Kasama <blizzard03@kasama>
2023-11-21 11:38:37 +0900

    Add a workflow

--- test.bash
+++ test.bash
@@ -1,5 +1,26 @@
-#!/bin/bash
+#!/bin/bash -xv
+#SPDX-FileCopyrightTest: 2023 Yuki Kasama
+#SPDX-Licence-Identifier: BSD-3-Clause
 
+ng () {
+        echo NG at Line $1
+	res=1
+}
+
+res=0
+
+### I/O
 out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng ${LINENO}
 
-[ "${out}" = 15 ]
+### STRANGE INPUT
+out=$(echo あ | ./plus)
+[ "$?" = 1 ] || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(echo  | ./plus)
+[ "$?" = 1 ] || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK
+exit $res
